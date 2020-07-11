9efefb5c33de6ec99e0fdd0742cadcfff7729b9c
lihuan <494397353@qq.com>
2020-07-12 00:21:51 +0800

    leetcode 239.滑动窗口最大值.js hard

---
+++ algorithm/leetcode/239.滑动窗口最大值.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=javascript
+ *
+ * [239] 滑动窗口最大值
+ *
+ * https://leetcode-cn.com/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (48.18%)
+ * Likes:    447
+ * Dislikes: 0
+ * Total Accepted:    59.5K
+ * Total Submissions: 123.5K
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+ * 个数字。滑动窗口每次只向右移动一位。
+ * 
+ * 返回滑动窗口中的最大值。
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你能在线性时间复杂度内解决此题吗？
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7] 
+ * 解释: 
+ * 
+ * ⁠ 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 1 <= k <= nums.length
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    if (nums.length === 1 || k === 1) {
+        return nums
+    }
+    let maxArr = []
+    let que = [] // 存储下标且队头始终是最大元素的下标
+    for (let i = 0; i < nums.length; i++) {
+        if (i - que[0] >= k) { // 滑动窗口外的元素直接从队头删除
+            que.shift()
+        }
+        while(nums[que[que.length - 1]] <= nums[i]) {
+            que.pop() // 新加入的数比原来的数都要大，则直接弹出
+        }
+        que.push(i)
+
+        if (i >= k-1) { // 第一个滑动窗口生成最大值是从k之前的元素生成的
+            maxArr.push(nums[que[0]])
+        }
+    }
+    return maxArr
+};
+// @lc code=end
+
