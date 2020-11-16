604780775d0328fc6dc7c0827b5077993ced1c46
lihuan <494397353@qq.com>
2020-11-16 15:56:12 +0800

    leetcode 1419 数青蛙 (minimum-number-of-frogs-croaking)

---
+++ algorithm/leetcode/1419.数青蛙.js
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=1419 lang=javascript
+ *
+ * [1419] 数青蛙
+ *
+ * https://leetcode-cn.com/problems/minimum-number-of-frogs-croaking/description/
+ *
+ * algorithms
+ * Medium (39.39%)
+ * Likes:    36
+ * Dislikes: 0
+ * Total Accepted:    5.9K
+ * Total Submissions: 14.9K
+ * Testcase Example:  '"croakcroak"'
+ *
+ * 给你一个字符串 croakOfFrogs，它表示不同青蛙发出的蛙鸣声（字符串 "croak" ）的组合。由于同一时间可以有多只青蛙呱呱作响，所以
+ * croakOfFrogs 中会混合多个 “croak” 。请你返回模拟字符串中所有蛙鸣所需不同青蛙的最少数目。
+ * 
+ * 注意：要想发出蛙鸣 "croak"，青蛙必须 依序 输出 ‘c’, ’r’, ’o’, ’a’, ’k’ 这 5
+ * 个字母。如果没有输出全部五个字母，那么它就不会发出声音。
+ * 
+ * 如果字符串 croakOfFrogs 不是由若干有效的 "croak" 字符混合而成，请返回 -1 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：croakOfFrogs = "croakcroak"
+ * 输出：1 
+ * 解释：一只青蛙 “呱呱” 两次
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：croakOfFrogs = "crcoakroak"
+ * 输出：2 
+ * 解释：最少需要两只青蛙，“呱呱” 声用黑体标注
+ * 第一只青蛙 "crcoakroak"
+ * 第二只青蛙 "crcoakroak"
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：croakOfFrogs = "croakcrook"
+ * 输出：-1
+ * 解释：给出的字符串不是 "croak" 的有效组合。
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：croakOfFrogs = "croakcroa"
+ * 输出：-1
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= croakOfFrogs.length <= 10^5
+ * 字符串中的字符只有 'c', 'r', 'o', 'a' 或者 'k'
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} croakOfFrogs
+ * @return {number}
+ */
+//一、计数法
+let minNumberOfFrogs = (croakOfFrogs) => {
+    let c = 0, r = 0, o = 0, a = 0, k = 0, cnt = 0
+    for (let x of croakOfFrogs) {
+        if (x == 'c') c++
+        else if (x == 'r') r++
+        else if (x == 'o') o++
+        else if (x == 'a') a++
+        else if (x == 'k') k++
+        else return -1
+        if (r > c || o > r || a > o || k > a) return -1
+        cnt = Math.max(c, cnt)
+        if (k == 1) {
+            c--
+            r--
+            o--
+            a--
+            k--
+        }
+    }
+    return (c || r || o || a || k) ? -1 : cnt
+};
+// @lc code=end
+
