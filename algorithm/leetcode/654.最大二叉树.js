048b3504153ed31b49ce38ae3745dc19f12fbcdc
lihuan <494397353@qq.com>
2020-11-17 11:02:35 +0800

    leetcode 654 最大二叉树

---
+++ algorithm/leetcode/654.最大二叉树.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=javascript
+ *
+ * [654] 最大二叉树
+ *
+ * https://leetcode-cn.com/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (81.55%)
+ * Likes:    215
+ * Dislikes: 0
+ * Total Accepted:    24.9K
+ * Total Submissions: 30.5K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 给定一个不含重复元素的整数数组。一个以此数组构建的最大二叉树定义如下：
+ * 
+ * 
+ * 二叉树的根是数组中的最大元素。
+ * 左子树是通过数组中最大值左边部分构造出的最大二叉树。
+ * 右子树是通过数组中最大值右边部分构造出的最大二叉树。
+ * 
+ * 
+ * 通过给定的数组构建最大二叉树，并且输出这个树的根节点。
+ * 
+ * 
+ * 
+ * 示例 ：
+ * 
+ * 输入：[3,2,1,6,0,5]
+ * 输出：返回下面这棵树的根节点：
+ * 
+ * ⁠     6
+ * ⁠   /   \
+ * ⁠  3     5
+ * ⁠   \    / 
+ * ⁠    2  0   
+ * ⁠      \
+ * ⁠       1
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 给定的数组的大小在 [1, 1000] 之间。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function (nums) {
+    if (nums.length === 0) {
+        return null
+    }
+    const big = Math.max(...nums)
+    const root = new TreeNode(big)
+    root.left = constructMaximumBinaryTree(nums.slice(0, nums.indexOf(big)))
+    root.right = constructMaximumBinaryTree(nums.slice(nums.indexOf(big) + 1))
+    return root
+};
+// @lc code=end
+
