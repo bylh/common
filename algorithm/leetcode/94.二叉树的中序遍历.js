81ebd86f2849159483f02c00d14adfd73f813c72
lihuan <494397353@qq.com>
2020-11-24 10:35:25 +0800

    leetcode 94 二叉树的中序遍历（迭代、递归分别实现）

---
+++ algorithm/leetcode/94.二叉树的中序遍历.js
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (73.74%)
+ * Likes:    780
+ * Dislikes: 0
+ * Total Accepted:    301.4K
+ * Total Submissions: 406.5K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// var inorderTraversal = function (root) { // 递归
+//     const arr = []
+//     if (root == null) {
+//         return [];
+//     }
+//     arr.push(...inorderTraversal(root.left))
+//     arr.push(root.val)
+//     arr.push(...inorderTraversal(root.right))
+//     return arr
+// };
+var inorderTraversal = function (root) { // 迭代
+    const list = []
+    const stack = [] // 使用栈来实现
+    let cur = root
+    while (cur != null || stack.length > 0) {
+        if (cur != null) {
+            stack.push(cur)
+            cur = cur.left
+        } else {
+            cur = stack.pop()
+            list.push(cur.val)
+            cur = cur.right
+        }
+    }
+    return list
+};
+// @lc code=end
+
