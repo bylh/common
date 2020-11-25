09ab0544a97a399eb9854a0b4833012365fb17ae
lihuan <494397353@qq.com>
2020-11-25 15:51:41 +0800

    leetcode 50 pow-x-n

--- algorithm/leetcode/50.pow-x-n.js
+++ algorithm/leetcode/50.pow-x-n.js
@@ -49,14 +49,14 @@
  * @return {number}
  */
 var myPow = function (x, n) {
-    let res = 1
-    for (let i = n; i != 0; i /= 2) {
-        if (i % 2 === 0) {
-            res *= x
-        }
-        x *= x
+    if (n === 0) return 1 // n=0直接返回1
+    if (n < 0) {   				//n<0时 x的n次方等于1除以x的-n次方分
+        return 1 / myPow(x, -n)
     }
-    return n < 0 ? 1 / res : res
-};
+    if (n % 2) {    //n是奇数时 x的n次方 = x*x的n-1次方
+        return x * myPow(x, n - 1)
+    }
+    return myPow(x * x, n / 2) //n是偶数，使用分治，一分为二，等于x*x的n/2次方 
+}
 // @lc code=end
 
