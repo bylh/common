6779e50dccb61e9f4576f2650fe3fdf4396e0cc1
lihuan <494397353@qq.com>
2020-12-14 22:42:30 +0800

    add webpack demo

---
+++ interview/webpack/webpack-demo/src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png';
+import Data from './data.xml';
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello') // 添加类名hello
+    // 将图像添加到我们现有的 div。
+    let myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    console.log(Data);
+    return element;
+}
+
+document.body.appendChild(component());
