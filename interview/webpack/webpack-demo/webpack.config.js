959304457bc913a86d67001e2c85f12d6a36ee23
lihuan <494397353@qq.com>
2020-12-14 22:46:19 +0800

    clear webpack demo file

--- interview/webpack/webpack-demo/webpack.config.js
+++ interview/webpack/webpack-demo/webpack.config.js
@@ -6,39 +6,40 @@ module.exports = {
         filename: 'bundle.js', // 出口文件
         path: path.resolve(__dirname, 'dist') // 出口文件在当前目录的dist文件夹下
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/, // 匹配.css结尾的文件
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
+    // to-clear文件夹资源用到的loader
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.css$/, // 匹配.css结尾的文件
+    //             use: [
+    //                 'style-loader',
+    //                 'css-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(png|svg|jpg|gif)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(csv|tsv)$/,
+    //             use: [
+    //                 'csv-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.xml$/,
+    //             use: [
+    //                 'xml-loader'
+    //             ]
+    //         }
+    //     ]
+    // }
 };
