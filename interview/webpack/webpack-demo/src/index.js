950ca48b381f5e654da1ee975b040821e3801ed1
lihuan <494397353@qq.com>
2020-12-15 00:12:21 +0800

    add webpack dev server

--- interview/webpack/webpack-demo/src/index.js
+++ interview/webpack/webpack-demo/src/index.js
@@ -1,13 +1,18 @@
 import _ from 'lodash'
+import printMe from './print'
 // TODO to-clear
 // import './style.css'
 // import Icon from './icon.png';
 // import Data from './data.xml';
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     // element.classList.add('hello') // 添加类名hello
     // 将图像添加到我们现有的 div。
     // let myIcon = new Image();
--- interview/webpack/webpack-demo/webpack.config.js
+++ interview/webpack/webpack-demo/webpack.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
+// 在目标文件夹(这里是dist)自动生成index.html， 并引用bundle输出文件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 清理dist文件夹
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+/*
+ [什么是manifest](https://www.webpackjs.com/concepts/manifest/)
+ */
 
 module.exports = {
-    entry: './src/index.js', // 入口文件
+    // entry: './src/index.js', // 入口文件
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: { // webpack-dev-server
+        contentBase: './dist',
+        open: true
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            title: 'Output Management' // 网页标题
+        }),
+    ],
     output: {
-        filename: 'bundle.js', // 出口文件
-        path: path.resolve(__dirname, 'dist') // 出口文件在当前目录的dist文件夹下
+        // filename: 'bundle.js', // 出口文件
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'), // 出口文件在当前目录的dist文件夹下
+        publicPath: '/',  // server 脚本使用 publicPath(webpack-dev-middleware server.js需要的)
     },
     // to-clear文件夹资源用到的loader
     // module: {
