618a79997e8e54ecc2c21dae54c69c511a985af4
lihuan <494397353@qq.com>
2020-12-19 00:58:22 +0800

    leetcode 40.组合总和-ii

--- algorithm/leetcode/40.组合总和-ii.js
+++ algorithm/leetcode/40.组合总和-ii.js
@@ -53,8 +53,39 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum2 = function(candidates, target) {
-    
+// TODO 去重效率低，待剪枝优化
+var combinationSum2 = function (candidates, target) {
+    const ans = []
+    const dfs = (target, combine, idx) => {
+        if (idx === candidates.length + 1) {
+            return
+        }
+        if (target === 0) {
+            // ans中如果包含了combine, 那么ans中必定有一项长度和combine一样，
+            // 并且包含元素也一样，这里不管顺序，即[1, 2, 3]和[3, 2, 1]鉴定为相同的
+
+            if (ans.some(arr => arr.length === combine.length
+                && arr.every(item => combine.indexOf(item) !== -1)
+                && combine.every(item => arr.indexOf(item) !== -1))) {
+                    // 重复了，所以什么都不做
+            } else {
+                ans.push(combine)
+            }
+            return
+        }
+        // 两种情况
+
+        // 1、直接跳过
+        dfs(target, combine, idx + 1)
+        // 2、当前重复继续
+        if (target - candidates[idx] >= 0) {
+            dfs(target - candidates[idx], [...combine, candidates[idx]], idx + 1)
+        }
+    }
+    dfs(target, [], 0)
+    return ans
 };
+// arr = [10, 1, 2, 7, 6, 1, 5]
+// console.log(combinationSum2(arr, 8))
 // @lc code=end
 
