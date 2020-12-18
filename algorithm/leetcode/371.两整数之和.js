fa821c327c7b5b94332bf1a2a28c8f9a6ee7d464
lihuan <494397353@qq.com>
2020-12-18 23:30:09 +0800

    leetcode 371 两树之和 TODO

---
+++ algorithm/leetcode/371.两整数之和.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=371 lang=javascript
+ *
+ * [371] 两整数之和
+ *
+ * https://leetcode-cn.com/problems/sum-of-two-integers/description/
+ *
+ * algorithms
+ * Easy (56.71%)
+ * Likes:    343
+ * Dislikes: 0
+ * Total Accepted:    41.9K
+ * Total Submissions: 73.6K
+ * Testcase Example:  '1\n2'
+ *
+ * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ * 
+ * 示例 1:
+ * 
+ * 输入: a = 1, b = 2
+ * 输出: 3
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: a = -2, b = 3
+ * 输出: 1
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function (a, b) {
+    let sum
+    while (b) {
+        sum = a ^ b
+        b = (a & b) << 1
+        a = sum
+    }
+    return sum
+};
+// @lc code=end
+
