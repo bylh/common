61716f69d4af3b5ae7b866bf83f3979ca20b51a4
lihuan <494397353@qq.com>
2020-12-24 21:55:27 +0800

    add cancel axios demo

---
+++ interview/1/cancelToken/cancelToken.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
+
+axios.get('/user/12345', {
+  cancelToken: source.token
+}).catch(function (thrown) {
+  if (axios.isCancel(thrown)) {
+    console.log('Request canceled', thrown.message);
+  } else {
+    // handle error
+  }
+});
+
+axios.post('/user/12345', {
+  name: 'new name'
+}, {
+  cancelToken: source.token
+})
+
+// cancel the request (the message parameter is optional)
+source.cancel('Operation canceled by the user.');
