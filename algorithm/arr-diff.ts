ffc1a2043a84dbdac750eeaaf0a0372a2af4a801
ferrisli <494397353@qq.com>
2021-10-19 10:13:37 +0800

    modify arr-diff

--- algorithm/arr-diff.ts
+++ algorithm/arr-diff.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env ts-node
 
-let a = new Set([1, 2, 3]);
-let b = new Set([3, 5, 2]); 
+let a = new Set(['P_0103', 'P_0104', 'P_0082', 'P_0105', 'P_0106', 'P_0146', 'P_0145', 'P_0143', 'P_0158']);
+let b = new Set(['P_0103', 'P_0104', 'P_0105', 'P_0106', 'P_0143', 'P_0145', 'P_0146', 'P_0158']); 
 
 // 并集
 let unionSet = new Set([...a, ...b]);
@@ -14,7 +14,8 @@ let intersectionSet = new Set([...a].filter(x => b.has(x)));
 // ab差集
 let differenceABSet = new Set([...a].filter(x => !b.has(x)));
 let differenceBASet = new Set([...b].filter(x => !a.has(x)));
-console.log(Array.from(differenceABSet), Array.from(differenceBASet));
+console.log('AB:', Array.from(differenceABSet));
+console.log('BA', Array.from(differenceBASet))
 
 // let a1 = ['A', 'B', 'C'];
 // let a2 = ['D', 'E', 'C', 'A'];
